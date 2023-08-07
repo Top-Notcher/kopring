7a04123fba8f3f8f9b3c19f55a0622a70e0e7e68
박현민 <dndb4902@naver.com>
2023-08-07 17:03:17 +0900

    #30. 조금 더 깔끔한 코드로 변경

--- src/main/kotlin/com/group/libraryapp/dto/user/response/UserLoanHistoryResponse.kt
+++ src/main/kotlin/com/group/libraryapp/dto/user/response/UserLoanHistoryResponse.kt
@@ -1,12 +1,32 @@
 package com.group.libraryapp.dto.user.response
 
+import com.group.libraryapp.domain.user.User
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory
+
 data class UserLoanHistoryResponse(
     val name: String,   // 유저 이름
     val books: List<BookHistoryResposne>
 ) {
+    companion object {
+        fun of(user: User): UserLoanHistoryResponse {
+            return UserLoanHistoryResponse(
+                name = user.name,
+                books = user.userLoanHistories.map(BookHistoryResposne::of)
+            )
+        }
+    }
 }
 
 data class BookHistoryResposne(
     val name: String,   // 책의 이름
     val isReturn: Boolean,
-)
+){
+    companion object {
+        fun of(history: UserLoanHistory): BookHistoryResposne {
+            return BookHistoryResposne(
+                name = history.bookName,
+                isReturn = history.isReturn
+            )
+        }
+    }
+}
